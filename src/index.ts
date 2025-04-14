9720a54b90b50c50b604338102c7b0ee65cae7e3
PhaserEditor2D <developers@phasereditor2d.com>
2025-04-14 03:05:44 -0400

    Add scene-move-objects-to-parent and scene-pack-objects-in-container tools

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,15 @@ defineTool("scene-delete-objects", "Delete the given objects from the scene.", {
     objectIds: z.array(z.string()).describe("The `id`s of the objects to delete.")
 });
 
+defineTool("scene-move-objects-to-parent", "Move the given objects to the given parent.", {
+    objectIds: z.array(z.string()).describe("The `id`s of the objects to move."),
+    parentId: z.string().optional().describe("The `id` of the parent to move the objects to. If no parent is given, the objects will be moved to the root of the scene."),
+});
+
+defineTool("scene-pack-objects-in-container", "Create a container to group the given objects. The container and objects are positioned so the container size is minimal.", {
+    objectIds: z.array(z.string()).describe("The `id`s of the objects to move."),
+});
+
 // Game Objects
 
 defineGameObjectTool("image", {
