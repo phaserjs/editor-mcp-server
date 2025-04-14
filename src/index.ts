3ce50eae9cc1c2b33819991bd741d80fec2ad4af
PhaserEditor2D <developers@phasereditor2d.com>
2025-04-14 01:18:07 -0400

    Adds layer and container support.

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,13 @@ defineTool("project-get-texture-binary", "Get the binary data of a texture.", {
 
 defineTool("scene-get-scene-dimension", "Get the dimensions of the current scene.", {});
 
+defineTool("scene-get-screenshot", "Get a screenshot of the scene.", {
+    x: z.number().describe("The x coordinate of the top left corner of the screenshot."),
+    y: z.number().describe("The y coordinate of the top left corner of the screenshot."),
+    width: z.number().describe("The width of the screenshot."),
+    height: z.number().describe("The height of the screenshot."),
+});
+
 defineTool("scene-get-scene-data", "Get all objects in the current scene, including their properties.", {});
 
 defineTool("scene-move-object-in-render-list", "Sort objects in the current scene.", {
@@ -31,43 +38,42 @@ defineTool("scene-move-object-in-render-list", "Sort objects in the current scen
 });
 
 defineTool("scene-delete-objects", "Delete the given objects from the scene.", {
-    objectIds: z.string().describe("The `id`s of the objects to delete.")
+    objectIds: z.array(z.string()).describe("The `id`s of the objects to delete.")
 });
-
-// TODO: maybe we don't need this since we provide the tool to get all the objects in the scene
-// defineTool("scene-get-object-properties", "Get properties of the given object.", {});
 
 // Game Objects
 
-// Image
-
-defineTool("scene-add-image", "Add an Image game object to the scene.", {
+defineGameObjectTool("image", {
     ...GameObjectComponent(),
     ...TransformComponent(),
     ...TextureComponent(),
 });
 
-defineTool("scene-update-image", "Set properties of the given Image game object in the scene.", {
-    id: z.string().describe("The `id` of the image to update."),
-    ...GameObjectComponent(),
-    ...TransformComponent(),
-    ...TextureComponent(),
-});
-
-// Text
-
-defineTool("scene-add-text", "Add a Text game object to the scene", {
+defineGameObjectTool("text", {
     ...GameObjectComponent(),
     ...TransformComponent(),
     ...TextComponent(),
 });
 
-defineTool("scene-update-text", "Set properties of the given Text game object in the scene.", {
-    id: z.string().describe("The `id` of the Text game object to update."),
+defineGameObjectTool("layer", {
     ...GameObjectComponent(),
     ...TransformComponent(),
-    ...TextComponent(),
 });
+
+defineGameObjectTool("container", {
+    ...GameObjectComponent(),
+    ...TransformComponent(),
+});
+
+function defineGameObjectTool(name: string, args: ZodRawShape) {
+
+    defineTool(`scene-add-${name}`, `Add a new ${name} game object to the scene.`, args);
+
+    defineTool(`scene-update-${name}`, `Update the given ${name} game object in the scene.`, {
+        id: z.string().describe(`The \`id\` of the ${name} game object to update.`),
+        ...args
+    });
+}
 
 function defineTool(name: string, description: string, args: ZodRawShape) {
 
@@ -83,44 +89,6 @@ function defineTool(name: string, description: string, args: ZodRawShape) {
         };
     });
 }
-
-// server.tool("make-rectangle-shape", "Make a rectangle shape with Phaser Editor", {
-//     x: z.number().describe("X position of the rectangle"),
-//     y: z.number().describe("Y position of the rectangle"),
-//     width: z.number().describe("Width of the rectangle"),
-//     height: z.number().describe("Height of the rectangle"),
-//     color: z.string().describe("Color of the rectangle. The color is in css hex format. For example: #FF0000"),
-// }, async (args) => {
-
-//     return {
-//         content: [
-//             {
-//                 type: "text",
-//                 text: `this.add.rectangle(${args.width}, ${args.height}, "${args.color}")`,
-//             }
-//         ]
-//     };
-// });
-
-// server.prompt("my-prompt-make-a-rect", "Make a rectangle shape with Phaser Editor, with an horizontal or vertical orientation", {
-//     orientation: z.enum(["horizontal", "vertical"]).describe("Orientation of the rectangle. It could be 'horizontal' or 'vertical'"),
-// }, async (args) => {
-
-//     const width = args.orientation === "horizontal" ? 100 : 50;
-//     const height = args.orientation === "horizontal" ? 50 : 100;
-
-//     return {
-//         messages: [
-//             {
-//                 role: "assistant",
-//                 content: {
-//                     type: "text",
-//                     text: `Create a rectangle with width ${width} and height ${height}`,
-//                 }
-//             }
-//         ]
-//     };
-// });
 
 async function main() {
 
