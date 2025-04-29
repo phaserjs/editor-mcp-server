ab929ba5e1bfc4463c748de304bf6db18a9a29c6
PhaserEditor2D <developers@phasereditor2d.com>
2025-04-29 14:10:16 -0400

    Adds tool to retrieve PNG image of a Spine skin with specified animation and skin name.

--- src/tools/assets-tools.ts
+++ src/tools/assets-tools.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { TextureComponent } from "../schemas/components.js";
+import { SpineComponent, TextureComponent } from "../schemas/components.js";
 import { defineTool } from "../utils.js";
 
 export function defineAssetTools() {
@@ -33,5 +33,12 @@ export function defineAssetTools() {
     defineTool("assets-get-spine-skeleton-info", "Get the skeleton info (animations and skins) of the given Spine skeleton key. To build the info of a skeleton it requires also the Spine atlas key. The user can select a different atlas key, but it is common that the user also define a 'preview' atlas key for each skeleton. You can use it if it exists or ask for a different atlas key to the user.", {
         dataKey: z.string().describe("The key of the spine data (skeleton) asset."),
         atlasKey: z.string().describe("The key of the spine atlas asset."),
+    });
+
+    defineTool("assets-get-spine-skin-image", "Get the PNG image of the given Spine skin. You can use this tool to get a better understanding of the appearance of a Spine skeleton with a given skin. The image may be scaled or not. You can the skeleton data to get more info about the size of a skeleton.", {
+        dataKey: z.string().describe("The key of the spine data (skeleton) asset."),
+        atlasKey: z.string().describe("The key of the spine atlas asset."),
+        skinName: z.string().describe("The name of the skin to use. A skeleton can have multiple skins."),
+        animationName: z.string().describe("The name of the animation to use. A skeleton can have multiple animations."),
     });
 }
